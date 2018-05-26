453d9a13dd15fed98c1fef86e7533506f37cdf3e
jadon <jadonclugston@gmail.com>
2018-05-26 13:34:12 -0400

    Add GUI class

---
+++ src/GUI.java
@@ -0,0 +1,10 @@
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+public class GUI extends Application {
+
+
+}
