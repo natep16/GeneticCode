72aae85214dd82b3865a85c2a5dda8f3c3f516c6
jadon <jadonclugston@gmail.com>
2018-05-27 18:46:10 -0400

    h

--- src/GUI.java
+++ src/GUI.java
@@ -4,7 +4,31 @@ import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
+import javafx.scene.control.*;
 public class GUI extends Application {
+    Button btn;
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override public void start(Stage primaryStage){
+        btn = new Button();
+        btn.setText("Analyze Genetic Code");
+        BorderPane pane = new BorderPane();
+        pane.setCenter(btn);
+        btn.setOnAction(e -> buttonClick());
+        Scene scene = new Scene(pane,480,720);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Genetic Code App");
+        primaryStage.show();
 
+    }
+    public void buttonClick(){
+        if(btn.getText().equals("Click me Please")){
+            btn.setText("You clicked me!");
+        }
+        else{
+            btn.setText("Click me Please");
+        }
+    }
 
 }
